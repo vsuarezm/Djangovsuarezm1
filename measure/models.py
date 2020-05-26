c4d564029161eef906b5ba1909fdcbd9069cea8f
Valeria <vsuarezm@eafit.edu.co>
2020-05-26 14:14:44 -0500

    Taller1

--- measure/models.py
+++ measure/models.py
@@ -4,7 +4,10 @@ from django.db import models
 import uuid
 class Measure(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    type = models.CharField(verbose_name='Tipo', max_length=20)
-    value = models.IntegerField(verbose_name='Valor')
+    codigo =  models.CharField(verbose_name ='codigo', max_length=40, default ='1')
+    nombre =  models.CharField(verbose_name ='nombre', max_length=40, default ='1')
+    descripcion =  models.CharField(verbose_name ='descripcion', max_length=40, default ='1')
+    tipoIndicador =  models.CharField(verbose_name ='tipoIndicador', max_length=40, default ='1')
+    prioridad =  models.CharField(verbose_name ='prioridad', max_length=40, default ='1')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- proyectointegrador1/settings.py
+++ proyectointegrador1/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'proyectointegrador1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'evergreen',                     
+        'USER': 'evergreen@serverevergreen',                     
+        'PASSWORD': 'zftUYR85', 
+        'HOST': 'serverevergreen.postgres.database.azure.com'
     }
 }
 
